7aecef17e1d226be74311b2fe1e771d4e2ff7997
Chris Huynh <cthuynh91@gmail.com>
2019-03-21 17:18:54 -0400

    data loading

--- mains/train.py
+++ mains/train.py
@@ -1,16 +1,25 @@
 """Main function for training."""
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 import torch
 import torch.nn as nn
-import torch.optim. as optim
+import torch.optim as optim
+from torch.utils.data import DataLoader
 
 from flow.models.u_net import UNet
+from flow.data_loaders.fetalsheepseg import FetalSheepSegDataset
 
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_dir = '/home/chris/flow/data/FetalSheepSegmentation/processed'
+    trainset = FetalSheepSegDataset(data_dir, train=True)
+    validset = FetalSheepSegDataset(data_dir, train=False)
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True)
+    validloader = DataLoader(validset, batch_size=4, shuffle=True)
 
     net = UNet()
 
@@ -19,12 +28,20 @@ def main():
 
     net.to(device)
 
-    max_epoch = 2
+    max_epoch = 1
 
     for epoch in range(max_epoch):
-        for minibatch in range(num_batch):
-            inputs, truth = get_next_batch()
-
+        print('Epoch # {}'.format(epoch + 1))
+        for i, minibatch in enumerate(trainloader):
+            inputs, truth = minibatch
+            print('Batch # {}'.format(i + 1))
+            print(type(inputs))
+            print(inputs.dtype)
+            print(inputs.shape)
+            print(type(truth))
+            print(truth.dtype)
+            print(truth.shape)
+            """
             inputs, truth = inputs.to(device), truth.to(device)
 
             optimizer.zero_grad()
@@ -34,7 +51,7 @@ def main():
             loss.backward()
 
             optimizer.step()
-
+            """
     return
 
 
