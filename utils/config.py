bef45551fec4af023b19f5cb755a7c137ea877e1
Chris Huynh <cthuynh91@gmail.com>
2019-03-22 00:21:44 -0400

    z

--- utils/config.py
+++ utils/config.py
@@ -1,19 +1,18 @@
-"""Class that reads .json configuration file."""
-import os
+"""Class that reads .json configuration file and sets attributes."""
 import json
 
 
-class NestedObject:
-    """For nesting attributes into Config class below."""
+class _NestedObject:
+    """For nesting objects as attributes of Config class."""
     def __init__(self, dictionary):
         for key, value in dictionary.items():
             if isinstance(value, dict):  # If nested dictionary
-                setattr(self, key, NestedObject(value))  # Nest an object
+                setattr(self, key, _NestedObject(value))  # Nest an object
             else:
                 setattr(self, key, value)  # Else set attribute to value
 
 
-class Config(NestedObject):
+class Config(_NestedObject):
     def __init__(self, json_file):
         self.json_file = json_file
 
